51d602a93581da649364a9b3995c343933b5ac4f
YUKUN-ZHANG1 <137257662+YUKUN-ZHANG1@users.noreply.github.com>
2024-09-29 17:46:50 -0700

    finished Color class

---
+++ Color.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Color{
+    public:
+    Color(int redValue = 0, int greenValue = 0, int blueValue = 0) 
+        :redValue(redValue), greenValue(greenValue), blueValue(blueValue){}
+
+    void setRedValue(int redValue){this->redValue = redValue;}
+    void setGreenValue(int greenValue){this->greenValue = greenValue;}
+    void setBlueValue(int blueValue){this->blueValue = blueValue;}
+
+    int getRedValue()const{return redValue;}
+    int getGreenValue()const{return greenValue;}
+    int getBlueValue()const{return blueValue;}
+
+    void print()const{
+        cout<<"R :"<<setw(4)<<redValue<<endl;
+        cout<<"G :"<<setw(4)<<greenValue<<endl;
+        cout<<"B :"<<setw(4)<<blueValue<<endl;
+    }
+
+    private:    
+    int redValue;
+    int greenValue;
+    int blueValue;
+};
